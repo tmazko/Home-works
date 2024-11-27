315e763c27e93143cc620a7ad98e47c9d8a39ff9
tmazko <tmazko@kse.org.ua>
2024-11-27 14:35:37 +0200

    updated code

--- hw_libraries.py
+++ hw_libraries.py
@@ -7,15 +7,23 @@ def quadratic_fun(x, a, b, c):
 def log2(x):
     return np.log2(x)
 
-x1 = np.linspace(-10, 10, 200)
-y1 = quadratic_fun(x1, 5, 2, -5)
+def draw(a,b,c):
+    x1 = np.linspace(-10, 10, 200)
+    y1 = quadratic_fun(x1, a, b, -c)
 
-x2 = np.logspace(-10, 1, 200)
-y2 = log2(x2)
+    x2 = np.logspace(-10, 1, 200)
+    y2 = log2(x2)
 
-mt.plot(x1, y1, label="Quadratic Function", color="blue")
-mt.plot(x2, y2, label="Logarithmic Function", color="red", linestyle="--")
+    mt.plot(x1, y1, label="Quadratic Function", color="blue")
+    mt.plot(x2, y2, label="Logarithmic Function", color="red", linestyle="--")
 
-mt.ylim(-10, 20)
-mt.legend()
-mt.show()
+    mt.ylim(-10, 20)
+    mt.legend()
+    mt.show()
+
+
+print("For function ax2+bx+c input a,b,c:")
+a=int(input("a = "))
+b=int(input("b = "))
+c=int(input("c = "))
+draw(a,b,c)
