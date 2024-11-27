d11dcf1eaeba8f6b1becd6f08d049d1b8b109557
tmazko <tmazko@kse.org.ua>
2024-11-27 14:22:04 +0200

    created log2(x) and used numpy

--- hw_libraries.py
+++ hw_libraries.py
@@ -1,13 +1,21 @@
-import matplotlib.pyplot as mp
+import matplotlib.pyplot as mt
+import numpy as np
 
-def quadratic_fun(x,a,b,c):
-    return a*x**2+b*x+c
+def quadratic_fun(x, a, b, c):
+    return a * x**2 + b * x + c
 
-x,y=[],[]
+def log2(x):
+    return np.log2(x)
 
-for i in range(-100,100):
-    x.append(i)
-    y.append(quadratic_fun(i,1,2,3))
+x1 = np.linspace(-10, 10, 200)
+y1 = quadratic_fun(x1, 5, 2, -5)
 
-mt.plot(x,y)
+x2 = np.logspace(-10, 1, 200)
+y2 = log2(x2)
+
+mt.plot(x1, y1, label="Quadratic Function", color="blue")
+mt.plot(x2, y2, label="Logarithmic Function", color="red", linestyle="--")
+
+mt.ylim(-10, 20)
+mt.legend()
 mt.show()
