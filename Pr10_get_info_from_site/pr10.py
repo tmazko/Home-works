d519df5b8a1660636eb5f68eaa76829fd4bec552
tmazko <tmazko@kse.org.ua>
2024-12-04 22:53:48 +0200

    cleaned code

--- Pr10_get_info_from_site/pr10.py
+++ Pr10_get_info_from_site/pr10.py
@@ -9,29 +9,29 @@ def write_in_csv(file_name, data, header):
         for std in data:
             spamwriter.writerow(std)
 
-
-resource=requests.get("https://data.uoi.ua/contest/uoi/2024/results")
-site =bs4.BeautifulSoup(resource.content, "html.parser")
-table_head = site.select(".table-striped > thead > tr th")
-table_body=site.select(".table-striped > tbody > tr")
-place_I=[]
-place_II=[]
-place_III=[]
-TABLE=[]
-row_head=[]
-
-for th in table_head:
-    row_head.append(th.text[:])
-
-
-for tr in table_body:
-    row=[]
-    for td in tr.select("td"):
-        row.append(td.text[:])
-    TABLE.append(row)
-print(row_head)
-for std in TABLE:
-    print(std)
+def load_data(site_path, class_name):
+    resource = requests.get(site_path)
+    site = bs4.BeautifulSoup(resource.content, "html.parser")
+    table_head = site.select(class_name+" > thead > tr th")
+    table_body = site.select(class_name+" > tbody > tr")
+    table = []
+    row_head = []
+    for th in table_head:
+        row_head.append(th.text[:])
+    for tr in table_body:
+        row = []
+        for td in tr.select("td"):
+            row.append(td.text[:])
+        table.append(row)
+    return row_head, table
+def print_std(place):
+    print(*row_head)
+    for std in place:
+        print(*std)
+row_head,TABLE=load_data("https://data.uoi.ua/contest/uoi/2024/results",".table-striped")
+place_I = []
+place_II = []
+place_III = []
 
 for std in TABLE:
     if std[-1]=="I":
@@ -52,3 +52,12 @@ for i in range(len(place_III)):
 write_in_csv("place_1.csv", place_I,row_head)
 write_in_csv("place_2.csv", place_II,row_head)
 write_in_csv("place_3.csv", place_III,row_head)
+
+
+
+print("         I place")
+print_std(place_I)
+print("         II place")
+print_std(place_II)
+print("         place III")
+print_std(place_III)
